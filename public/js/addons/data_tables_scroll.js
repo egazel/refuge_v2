b3154683d9f049fb416782e704bada01e1e2fa12
Elie Gazel <elie@gazel.net>
2020-06-14 20:14:38 +0200

    fixing js for dataTables

--- public/js/addons/data_tables_scroll.js
+++
@@ -1,9 +0,0 @@
-<script>
-    $(document).ready(function () {
-        $('#selectedColumn').DataTable({
-            "scrollY": "350px",
-            "scrollCollapse": true,
-        });
-        $('.dataTables_length').addClass('bs-select');
-    });
-</script>
