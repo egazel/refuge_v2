c1a8f3df4942db31402afcbd51b3024e3de491f8
Elie Gazel <elie@gazel.net>
2020-06-17 19:51:31 +0200

    enable CORS (nelmio cors)

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
